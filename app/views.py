652c1c4d418e441cb1c308c7207aa9b0fd6aabd6
Jonas Lim <jonaslwq@gmail.com>
2022-04-07 23:02:28 +0800

    Final

--- app/views.py
+++ app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.db import connection
+from django.contrib.auth import authenticate, login, logout
 
 # # Create your views here.
 # def index(request):
